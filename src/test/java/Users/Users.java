7479a109704678daf2af9f5c022efb122d548297
sabinapiekarska <spiekarska@o2.pl>
2018-06-14 08:33:13 +0200

    Logon sysadm + add cabinet

---
+++ src/test/java/Users/Users.java
@@ -0,0 +1,79 @@
+package Users;
+
+public class Users {
+    private String sysadm = "sysadm";
+    private String user10 = "user10";
+    private String user09 = "user09";
+    private String user08 = "user08";
+    private String user07 = "user07";
+    private String user06 = "user06";
+    private String user05 = "user05";
+    private String user04 = "user04";
+    private String user03 = "user03";
+    private String user02 = "user02";
+    private String user01 = "user01";
+    private String user00 = "user00";
+    private String user13 = "user13";
+    private String password = "asdqwe";
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSysadm() {
+        return sysadm;
+    }
+
+    public String getUser10() {
+        return user10;
+    }
+
+    public String getUser09() {
+        return user09;
+    }
+
+    public String getUser08() {
+        return user08;
+    }
+
+    public String getUser07() {
+        return user07;
+    }
+
+    public String getUser06() {
+        return user06;
+    }
+
+    public String getUser05() {
+        return user05;
+    }
+
+    public String getUser04() {
+        return user04;
+    }
+
+    public String getUser03() {
+        return user03;
+    }
+
+    public String getUser02() {
+        return user02;
+    }
+
+    public String getUser01() {
+        return user01;
+    }
+
+    public String getUser00() {
+        return user00;
+    }
+
+    public String getUser13() {
+        return user13;
+    }
+}
