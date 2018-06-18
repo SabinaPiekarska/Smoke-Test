582038ef889d84cb99cf2fab2518a3ae02859db4
sabinapiekarska <spiekarska@o2.pl>
2018-06-18 10:13:45 +0200

    Search

---
+++ src/test/java/Credentials/SearchQueries.java
@@ -0,0 +1,37 @@
+package Credentials;
+
+public class SearchQueries {
+    private String andOperator(){
+        String andOperator = "love AND tears";
+        return andOperator;
+    }
+    private String notOperator(){
+        String notOperator = "love not tears";
+        return notOperator;
+    }
+    private String orOperator(){
+        String orOperator = "fire and not eyes";
+        return orOperator;
+    }
+    private String wAndOperator(){
+        String wAndOperator = "thing* and w/3 lost";
+        return wAndOperator;
+    }
+    private String xFirtWordOperator(){
+        String andNotOperator = "things w/1 xfirstword";
+        return andNotOperator;
+    }
+    private String questionMarkOperator(){
+        String questionMarkOperator = "pompei?";
+        return questionMarkOperator;
+    }
+    private String xFirtWordOperator(){
+        String andNotOperator = "things w/1 xfirstword";
+        return andNotOperator;
+    }
+    private String xFirtWordOperator(){
+        String andNotOperator = "things w/1 xfirstword";
+        return andNotOperator;
+    }
+
+}
