ccee04fa5c3ea9dc2c0d62d545fe5009709ad237
sabinapiekarska <spiekarska@o2.pl>
2018-06-18 11:02:50 +0200

    Search operators

--- src/test/java/Credentials/SearchQueries.java
+++ src/test/java/Credentials/SearchQueries.java
@@ -13,9 +13,9 @@ public class SearchQueries {
         String orOperator = "fire and not eyes";
         return orOperator;
     }
-    private String wAndOperator(){
-        String wAndOperator = "thing* and w/3 lost";
-        return wAndOperator;
+    private String wAndAsteriskOperator(){
+        String wAndAsteriskOperator = "thing* and w/3 lost";
+        return wAndAsteriskOperator;
     }
     private String xFirtWordOperator(){
         String andNotOperator = "things w/1 xfirstword";
@@ -25,13 +25,17 @@ public class SearchQueries {
         String questionMarkOperator = "pompei?";
         return questionMarkOperator;
     }
-    private String xFirtWordOperator(){
-        String andNotOperator = "things w/1 xfirstword";
-        return andNotOperator;
+    private String equalOperator(){
+        String equalOperator = "ccitt_=";
+        return equalOperator;
     }
-    private String xFirtWordOperator(){
-        String andNotOperator = "things w/1 xfirstword";
-        return andNotOperator;
+    private String ampersandOperator(){
+        String ampersandOperator = "fast&";
+        return ampersandOperator;
     }
+     private String noiseWordsOperator(){
+            String noiseWordsOperator = "a about after all also an and another any are as at be because been before being between both but by came can come could did do each even for from further furthermore get got had has have he her here hi him himself how however i if in indeed into is it its just like made many me might more moreover most much must my never not now of on only or other our out over take than that the their them then there therefore these they this those through thus to too under up very was way we well were what when where which while who will with would you your";
+            return noiseWordsOperator;
+        }
 
 }
