152b456bfb9ad4a3f4852ff5f088b9d517d8a645
sabinapiekarska <spiekarska@o2.pl>
2018-06-19 13:58:02 +0200

    Xpaths

--- src/test/java/Test/Search/AdvancedSearch.java
+++ src/test/java/Test/Search/AdvancedSearch.java
@@ -19,12 +19,23 @@ public class AdvancedSearch extends Driver {
         getAdvancedSearchLink();
         waitUntilPageLoads(By.id("ctl00_ctl00_c_c_SearchBar_DataList_ctl00_ctrl_0_SAoF_cont_Text"));
         action.moveToElement(mapping.getFileOrMetadata());
-        action.moveToElement(mapping.getCheckOutStatus());
+//        action.moveToElement(mapping.getCheckOutStatus());
+//        action.click().build().perform();
+//        waitUntilPageLoads(By.id("ctl00_ctl00_c_c_SearchBar_DataList_ctl00_ctrl_0_COS_eq_SDDM_Arrow"));
+//        mapping.getDrpDownMenuArrow().click();
+//        action.moveToElement(mapping.getCheckIn());
+//        action.click().build().perform();
+
+        action.moveToElement(mapping.getFileDate());
         action.click().build().perform();
-        waitUntilPageLoads(By.id("ctl00_ctl00_c_c_SearchBar_DataList_ctl00_ctrl_0_COS_eq_SDDM_Arrow"));
-        mapping.getDrpDownMenuArrow().click();
-        action.moveToElement(mapping.getCheckIn());
+        action.moveToElement(mapping.getLibraryLocation());
         action.click().build().perform();
-        mapping.getAdvancedSearchButton().click();
+        action.moveToElement(mapping.getOwnedByMe());
+        action.click().build().perform();
+        action.moveToElement(mapping.getWorkflowStatus());
+        action.click().build().perform();
+        action.moveToElement(mapping.getPerson());
+        action.click().build().perform();
+//        mapping.getAdvancedSearchButton().click();
     }
 }
